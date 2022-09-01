394754cf50261d7bfa690bab3a154ddba81369c1
Nischay Sharma <nischay876@gmail.com>
2022-09-02 01:24:38 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -5,7 +5,7 @@ set -e
 while true
 do
   ffmpeg -loglevel error -y -re \
-    -re -fflags +genpts -stream_loop -1 -i ss.mp4 \
+    -re -fflags +genpts -stream_loop -1 -i Sakura-Girl.mp4 \
     -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
