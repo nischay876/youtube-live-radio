dd5a8f1a2cff1e539cc6eec041cdf93dbdcbcb83
Nischay Sharma <nischay876@gmail.com>
2022-09-04 17:42:55 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -7,8 +7,8 @@ do
   ffmpeg -loglevel error -y -re \
     -re  -stats -fflags +genpts -stream_loop -1 -i Sakura-Girl.mp4 \
     -f concat -safe 0 -i <((for f in ./*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf)  \
-    -c:v libx264 -preset ultrafast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -threads 0.5 -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
+    -c:v libx264 -preset ultrafast -b:v 1500k -maxrate 1500k -bufsize 2000k \
+    -threads 1 -framerate 20 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
     -c:a aac -b:a 128k -ar 44100 \
     -f flv rtmp://a.rtmp.youtube.com/live2/jq46-gj64-gwx1-sgqq-1k8c
 done
